6a74f979b40cb5709b5cd7b54295ec72b937dd31
Derek Blasko <100708408+derekblasko@users.noreply.github.com>
2022-04-20 17:37:02 -0400

    Add files via upload

---
+++ node.py
@@ -0,0 +1,10 @@
+from transaction_pool import transaction_pool
+from wallet import wallet
+from blockchain import blockchain
+
+class node():
+    
+    def __init__(self):
+        self.Transaction_pool = transaction_pool()
+        self.Wallet = wallet()
+        self.Blockchain = blockchain()
